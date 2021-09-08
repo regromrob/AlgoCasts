8319bf24df6e7d0e5989d194832150e89ac4bc2c
regromrob <romae31@gmail.com>
2021-09-08 00:25:47 -0400

    reverse a string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,32 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+//EXAMPLE 1
+// function reverse(str) {
+//    return str
+// .split('')
+// .reverse()
+// .join('')
+// }
+
+//EXAMPLE 2 NO USING REVERSE FUNCTION
+// function reverse(str) {
+//     //create an empty string called 'reversed'
+//     let reversed = ''
+//     //for each char in string - take char and add it to the start of 'reversed'
+//     for(let char of str){
+//         reversed = char + reversed
+//     }
+//     //return the variable 'reversed'
+//     return reversed
+//  }
+
+//EXAMPLE 3
+function reverse(str){
+    //transform string to array using split
+    return str.split('')
+    //use reduce helper function to condense to one value..our str reversed
+    .reduce((reversed, char)=>char + reversed, '')
+}
 
 module.exports = reverse;
