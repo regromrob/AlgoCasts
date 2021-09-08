bbd258c2537e2c9c48dcd59d8b089123a1eef186
regromrob <romae31@gmail.com>
2021-09-08 00:25:35 -0400

    reverse an integer

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,14 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    //take n and treat it as string by turning it into a string to then split into array
+    const reversedNum = n.toString()
+    .split('')
+    .reverse()
+    .join('')
+
+    return parseInt(reversedNum) * Math.sign(n)
+}
 
 module.exports = reverseInt;
